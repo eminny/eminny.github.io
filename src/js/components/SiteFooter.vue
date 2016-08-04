4c7b2408a7149eaf400cd15baa472cd28ae05c24
Alex Salisbury <asalisbury@gmail.com>
2016-08-04 18:18:51 -0400

    Scaffold footer/newsletter

--- src/js/components/SiteFooter.vue
+++ src/js/components/SiteFooter.vue
@@ -1,13 +1,18 @@
 <template>
-  <footer>
-    <p>Sign up and stay updated</p>
-    <form>
-
+  <footer class="site-footer">
+    <form id="newsletter__form"
+          action="http://xxxxx.us#.list-manage1.com/subscribe/post-json?u=xxxxx&id=xxxx&c=?"
+          method="get"
+    >
+      <label for="newsletter__email" class="newsletter__label">Sign up and stay updated</label>
+      <input id="newsletter__email" class="newsletter__email" type="email" placeholder="EMAIL ADDRESS">
+      <button class="newsletter__form__btn-submit" type="submit">Submit</button>
     </form>
+    <p class="site-footer__copyright">&copy; 2016 CANDER PARIS. <a v-link="{ name: 'terms' }">Terms and Conditions</a></p>
   </footer>
 </template>
 
-<style lang="sass" scoped>
+<style lang="sass">
     @import 'stylesheets/site-footer';
 </style>
 
