c10482c9579d391efe93fcf054c13edbdcabc52d
Alex Salisbury <asalisbury@gmail.com>
2016-09-15 23:39:58 -0400

    Tweak layout settings due to content size switch

--- src/js/components/SectionAbout.vue
+++ src/js/components/SectionAbout.vue
@@ -3,18 +3,20 @@
     <div class="single-page__content">
       <div class="section-about__intro">
         <div class="section-about__intro__image-wrap">
-          <img src="/images/about-intro-elizabeth-minett.jpg" alt="BY ELIZABETH MINETT">
+          <img src="/images/about-intro-elizabeth-minett.jpg" alt="BY ELIZABETH MINETT" class="section-about__intro__image">
         </div>
         <div class="section-about__intro__text-wrap">
-          <div class="section-about__intro__text-wrap__inner" v-if="language === 'en'" transition="fade">
+          <div class="section-about__intro__text-wrap__inner" v-show="language === 'en'" transition="fade">
             <h1 class="zeta">by Elizabeth Minett</h1>
             <h2 class="gamma">Cander candles blends essential oils and fragrances in a soy based candle to create an elevated experience.</h2>
-            <h3 class="p-beta">Founded by Elizabeth Minett, each and every candle fragrance is exquisitely crafted. Content about the brand, story, vision, and/or mission of the company. Luxury candles providing and enhancing a refined lifestyle.</h3>
+            <h3 class="p-beta">Cander Paris was founded in 2016 by Elizabeth Minett. Elizabeth was born and raised in a place where candor was the essence of her culture. A childhood filled with the rustic landscapes of harsh winters would forever shape her olfactive tastes. She developed a more refined nose during her years travelling the globe as a bon vivant. Cander Paris is inspired not only by a passion for luxury ambiance, but by the quest for genuine expression. She has teamed with world-renowned French perfumer Emmanuel Philip to create her debut, Scent One. Made in France with the excellence and tradition of French perfumery, the candles are exquisitely crafted of the highest quality. Challenging, yet euphorically indulgent.</h3>
+            <h3 class="p-beta">Elizabeth was born and raised near Toronto, Canada of Norwegian and Russian heritage. After a chance encounter in an elevator, she was scouted to work as a fashion model and soon moved to Europe. She was an early adopter of the digital influencer trend, and transitioned from the modelling world to create her successful lifestyle website HautAppetit. com. An auspicious visit to her friend’s perfume lab set her heart aflame for her next evolution as a scent creator. She is one of the youngest female entrepreneurs to have created her own luxury home scent brand. Elizabeth is regularly featured in online publications like Vogue, Elle, Harper’s Bazaar, and Grazia for her style and influence.</h3>
           </div>
-          <div class="section-about__intro__text-wrap__inner" v-if="language === 'fr'" transition="fade">
+          <div class="section-about__intro__text-wrap__inner" v-show="language === 'fr'" transition="fade">
             <h1 class="zeta">par Elizabeth Minett</h1>
             <h2 class="gamma">Bougies Cander mélanges essentiels parfum des sables bitumineux Sina bougie à base de soja pour créer une expérience de presse élevée.</h2>
-            <h3 class="p-beta">Fondée par Elizabeth Minett, chaque parfum de bougie est délicieusement conçu. Contenu sur la marque, l'histoire, la vision et / ou de la mission de l'entreprise. Bougies de luxe fournir et améliorer un mode de vie raffiné.</h3>
+            <h3 class="p-beta">La maison CANDER Paris a été fondée en 2016 par Elizabeth Minett. C’est au Canada qu’Elizabeth a été élevée dans la plus grande ingénuité. Elle garde de son enfance les souvenirs olfactifs d’une nature sauvage, de paysages rustiques marqués par les saisons. Epicurienne, elle affine progressivement ses capacités sensorielles lors de ses nombreux voyages. Plus qu’une maison de luxe, CANDER Paris est ainsi le fruit d’une véritable quête d’authenticité. C’est la raison pour laquelle Elizabeth a choisi de s’associer au talentueux parfumeur français Emmanuel Philip pour la création de sa première fragrance, Scent One. Résultat d’une confection délicate, les bougies CANDER sont fabriquées en France dans le plus pur respect des traditions et savoir-faire de la parfumerie de luxe.</h3>
+            <h3 class="p-beta">Elizabeth nait et grandit au Canada près de Toronto où elle bénéficie également d’une culture russe et norvégienne par ses parents. Pendant son adolescence, alors qu’elle se trouve dans un ascenseur, Elizabeth se fait repérer pour devenir mannequin et part rapidement en Europe pour lancer sa carrière. Adepte du monde digital et consciente de l’impact des « influenceurs », elle se transforme en bloggeuse lifestyle et lance avec succès son propre site, HautAppetit.com. C’est finalement au cours d’une visite dans l’atelier de l’un de ses amis parfumeur que se révèle sa vocation de créatrice de parfums. Elle devient ainsi la plus jeune entrepreneur dans le monde de la parfumerie d’ambiance de luxe. Avec son style unique, Elizabeth est souvent représentée dans des publications prestigieuses telles que Vogue, Elle, Harper’s Bazaar ou encore Grazia.</h3>
           </div>
           <div class="section-about__intro__language-switch">
             <a href="#" @click.prevent="setLanguage('en')" class="{{ language === 'en' ? 'is-active' : '' }}">EN</a>
@@ -37,7 +39,7 @@
     flex-direction: column;
     justify-content: flex-start;
     @media screen and ($large-screens) {
-      align-items: center;
+      align-items: flex-start;
       flex-direction: row;
       justify-content: center;
     }
@@ -55,8 +57,12 @@
   .section-about__intro__image-wrap {
     @media screen and ($large-screens) {
       padding-right: 0;
-      align-items: flex-end;
+      align-items: flex-start;
     }
+  }
+
+  .section-about__intro__image {
+    margin-top: 4rem;
   }
 
   .section-about__intro__text-wrap {
@@ -114,9 +120,12 @@
 </style>
 
 <script>
+  import { map } from 'lodash'
+  import scroll from 'scroll'
+  import store from '../store'
   import Diary from './Diary.vue'
   import SiteFooter from './SiteFooter.vue'
-  import store from '../store.js'
+  const page = require('scroll-doc')()
 
   export default {
     data () {
@@ -131,6 +140,10 @@
     methods: {
       setLanguage (lang) {
         this.language = lang
+        this.scrollToTop()
+      },
+      scrollToTop () {
+        scroll.top(page, 0, { duration: 400 })
       },
     },
     ready () {},
