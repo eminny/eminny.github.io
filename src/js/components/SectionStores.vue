7e5e44bfb245f3554343ee86702341b309d0a467
Alex Salisbury <asalisbury@gmail.com>
2016-09-15 23:40:16 -0400

    Remove image mistakenly added

--- src/js/components/SectionStores.vue
+++ src/js/components/SectionStores.vue
@@ -20,7 +20,6 @@
       </ul>
       <div v-if="!retailers.length">
         <p class="retailers__empty-state-message">Coming Soon</p>
-        <img src="/images/product-photo1.jpg" alt="CANDER PARIS">
       </div>
   </section>
     <site-footer></site-footer>
